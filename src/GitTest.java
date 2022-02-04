7c108e9917437aa9b184a4dab52490420cd11df4
Zeeshan <zeeshan1khan@gmail.com>
2022-02-04 10:25:14 +0530

    Added semicolon becuase it was missed by my colleague

--- src/GitTest.java
+++ src/GitTest.java
@@ -4,6 +4,6 @@ public class GitTest {
     {
         System.out.println("Hello World");
         System.out.println("2nd Line");
-        System.out.println("3rd Line")
+        System.out.println("3rd Line");
     }
 }
